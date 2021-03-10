46e219e98c67d9b69f19edbd55b396222c74daff
Amanda Edgar <plainjane916@gmail.com>
2021-03-10 11:34:12 -0600

    initial commit

---
+++ Meal-Tracking-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Meal_Tracking_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
