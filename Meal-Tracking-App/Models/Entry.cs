2748a4ddda9f48e32820b729ac63fa59939cad29
Amanda Edgar <plainjane916@gmail.com>
2021-03-18 17:47:17 -0500

    Created EntriesController and Entry Model

---
+++ Meal-Tracking-App/Models/Entry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Meal_Tracking_App.Models
+{
+    public class Entry
+    {
+        public int Date { get; set; }
+        public int Time { get; set; }
+        public string Description { get; set; }
+        public string Feelings { get; set; }
+
+        public int Id { get; }
+        static private int nextId = 1;
+
+        public Entry(int date, int time, string description, string feelings)
+        {
+            Date = date;
+            Time = time;
+            Description = description;
+            Feelings = feelings;
+            Id = nextId;
+            nextId++;
+        }
+    }
+}
