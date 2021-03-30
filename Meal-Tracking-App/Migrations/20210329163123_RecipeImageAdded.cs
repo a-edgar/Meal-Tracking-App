2afa8ec67653249cee9cddfc37bb2e31872a7523
Amanda Edgar <plainjane916@gmail.com>
2021-03-29 19:19:43 -0500

    Updated edit recipe functionality.

---
+++ Meal-Tracking-App/Migrations/20210329163123_RecipeImageAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Meal_Tracking_App.Migrations
+{
+    public partial class RecipeImageAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Recipes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Recipes");
+        }
+    }
+}
